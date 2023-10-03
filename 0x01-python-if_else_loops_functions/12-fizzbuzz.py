d52921f2e0dcc0f1fc2ffda3aabbf78f7256ecf6
zoraaga1 <fati.sousse12@gmail.com>
2023-10-03 23:48:42 +0100

    task 12

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -2,5 +2,5 @@
 
 def fizzbuzz():
     for i in range(1, 101):
-        print(("Fizz" * (i % 3 == 0) +
-               "Buzz" * (i % 5 == 0) or str(i)) + " ", end="")
+        print(("Fizz" * (i % 3 == 0) + "Buzz" * (i % 5 == 0) or
+               str(i)) + " ", end="")
