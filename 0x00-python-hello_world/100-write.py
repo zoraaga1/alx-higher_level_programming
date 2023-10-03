8c57d5cd7b58f74d04dd7e260a62357ff2f8d75b
zoraaga1 <fati.sousse12@gmail.com>
2023-10-03 02:48:09 +0100

    task 11

---
+++ 0x00-python-hello_world/100-write.py
@@ -0,0 +1,9 @@
+import sys
+
+def main():
+    message = "and that piece of art is useful - Dora Korpar, 2015-10-19"
+    sys.stderr.write(message + '\n')
+    sys.exit(1)
+
+if __name__ == "__main__":
+    main()
