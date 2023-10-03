3431454b16bdf8c47744ea6ac23a9644a168566c
zoraaga1 <133988175+zoraaga1@users.noreply.github.com>
2023-10-03 18:26:34 +0100

    Update 0-positive_or_negative.py

--- 0-positive_or_negative.py
+++ 0-positive_or_negative.py
@@ -1,7 +1,1 @@
-#!/usr/bin/python3
-import random
-number = random.randint(-10, 10)
-if number != 0:
-    if number < 0: print(f"{number} is negative")
-    else: print(f"{number} is postive")
-else: print(f"{number} is zero")
+
