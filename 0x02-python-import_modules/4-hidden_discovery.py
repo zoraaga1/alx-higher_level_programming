bc6cadbe530e23fc8cac1f079eee488b83562eac
zoraaga1 <fati.sousse12@gmail.com>
2023-10-06 00:36:18 +0100

    task 4

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 import hidden_4
 
+
 def main():
     module_names = dir(hidden_4)
     module_names.sort()  # Sort the names alphabetically
@@ -8,6 +9,7 @@ def main():
     for name in module_names:
         if not name.startswith("__"):  # Filter out names starting with "__"
             print(name)
+
 
 if __name__ == "__main__":
     main()
