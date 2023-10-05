1aaaa498782f6c01387a5331873da11bc5cc9090
zoraaga1 <fati.sousse12@gmail.com>
2023-10-06 00:34:51 +0100

    task 4

---
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+import hidden_4
+
+def main():
+    module_names = dir(hidden_4)
+    module_names.sort()  # Sort the names alphabetically
+
+    for name in module_names:
+        if not name.startswith("__"):  # Filter out names starting with "__"
+            print(name)
+
+if __name__ == "__main__":
+    main()
