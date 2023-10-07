41f98eb3c398079462b301bb27fe55324bb13888
zoraaga1 <fati.sousse12@gmail.com>
2023-10-07 13:43:30 +0100

    fisrt task

--- 0x03-python-data_structures/6-main.py
+++ 0x03-python-data_structures/6-main.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-print_matrix_integer = __import__('6-print_matrix_integer').print_matrix_integer
+print_matrix_integer = __import__('6-print_matrix_integer').print_matrix_integegit r
 
 matrix = [
     [1, 2, 3],
