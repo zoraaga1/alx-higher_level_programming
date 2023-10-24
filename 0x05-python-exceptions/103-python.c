7b61b42af9fade2e2aaad78f3d8b6fff49e91f0e
zoraaga1 <fati.sousse12@gmail.com>
2023-10-24 01:39:18 +0100

    task 10

--- 0x05-python-exceptions/103-python.c
+++ 0x05-python-exceptions/103-python.c
@@ -1,6 +1,10 @@
 #include <Python.h>
 #include <stdio.h>
 
+void print_python_list(PyObject *p);
+void print_python_bytes(PyObject *p);
+void print_python_float(PyObject *p);
+
 /**
  * print_python_list - Prints information about a Python list object.
  * @p: A pointer to the Python list object.
