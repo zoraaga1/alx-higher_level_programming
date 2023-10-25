dc2d5889b359661900ba62c0d2b09b896bde6b23
zoraaga1 <fati.sousse12@gmail.com>
2023-10-25 17:44:45 +0100

    task 7

---
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -0,0 +1,97 @@
+#!/usr/bin/python3
+"""Create two classes called Node and SinglyLinkedList"""
+
+
+class Node:
+    """
+    Defines a node of a singly linked list by:
+        Private instance attribute: data:
+            property def data(self): to retrieve it
+            property setter def data(self, value): to set it:
+                data must be an integer,
+                otherwise raise a TypeError exception
+                with the message data must be an integer
+        Private instance attribute: next_node:
+            property def next_node(self): to retrieve it
+            property setter def next_node(self, value):
+            to set it:
+                next_node can be None or must be a Node,
+                otherwise raise a TypeError exception with the message
+                next_node must be a Node object
+        Instantiation with data and next_node:
+        def __init__(self, data, next_node=None):
+    """
+    def __init__(self, data, next_node=None):
+        """Initializes the data of the node."""
+        self.data = data
+        self.next_node = next_node
+
+    @property
+    def data(self):
+        """Retrieves the data from the node."""
+        return self.__data
+
+    @data.setter
+    def data(self, value):
+        """Sets the data into a node."""
+        if not isinstance(value, int):
+            raise TypeError("data must be an integer")
+        self.__data = value
+
+    @property
+    def next_node(self):
+        """Retrieves the next node"""
+        return self.__next_node
+
+    @next_node.setter
+    def next_node(self, value):
+        """Sets the next node"""
+        if not isinstance(value, Node) and value is not None:
+            raise TypeError("next_node must be a Node object")
+        self.__next_node = value
+
+
+class SinglyLinkedList:
+    """
+    Defines a singly linked list by:
+        Private instance attribute: head (no setter or getter)
+        Simple instantiation: def __init__(self):
+        Should be printable:
+            print the entire list in stdout
+            one node number by line
+        Public instance method: def sorted_insert(self, value):
+        that inserts a new Node into the correct sorted position in the list
+        (increasing order)
+    """
+    def __init__(self):
+        """Initializes the linked list."""
+        self.head = None
+
+    def __str__(self):
+        """For the print statement in the main file."""
+        my_str = ""
+        node = self.head
+        while node:
+            my_str += str(node.data)
+            my_str += '\n'
+            node = node.next_node
+        return my_str[:-1]
+
+    def sorted_insert(self, value):
+        """Inserts a node in a sorted linked list."""
+        new_node = Node(value)
+
+        if self.head is None:
+            self.head = new_node
+            return
+
+        if value < self.head.data:
+            new_node.next_node = self.head
+            self.head = new_node
+            return
+
+        node = self.head
+        while node.next_node and node.next_node.data < value:
+            node = node.next_node
+        new_node.next_node = node.next_node
+        node.next_node = new_node
