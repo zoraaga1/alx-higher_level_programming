1de0b23b7c91f3082eabf1aeb1f357c4eadc7847
zoraaga1 <fati.sousse12@gmail.com>
2023-11-02 01:12:42 +0100

    add test case

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -2,7 +2,7 @@
 """Unittest for max_integer(list=[])
 """
 import unittest
-from max_integer import max_integer
+max_integer = __import__('6-max_integer').max_integer
 
 class TestMaxInteger(unittest.TestCase):
     def test_regular_list(self):
