cbdb281aa763de1a0351b7e7aa859d7aa414228d
zoraaga1 <fati.sousse12@gmail.com>
2023-11-02 01:14:26 +0100

    add test case

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -3,7 +3,6 @@
 a text with 2 new lines
 after each of these characters: ., ? and :"""
 
-
 def text_indentation(text):
     """Print 2 new lines after .,?"""
     str_error = "text must be a string"
@@ -11,10 +10,18 @@ def text_indentation(text):
     flag = False
     if type(text) is not str:
         raise TypeError(str_error)
-    new_text = ""
-    for char in text:
-        new_text += char
+    new_text = text.replace(". ", ".")
+    new_text = new_text.replace(": ", ":")
+    new_text = new_text.replace("? ", "?")
+    for char in new_text:
         if char in [".", "?", ":"]:
-            new_text += "\n\n"
-
-    print(new_text)
+            print(char)
+            print()
+            flag = True
+        else:
+            if flag is False:
+                print(char, end="")
+            else:
+                if char != " ":
+                    print(char, end="")
+                    flag = False
