3fea0b7eb4714e5031a407ac59723767090266d3
zoraaga1 <fati.sousse12@gmail.com>
2023-11-02 01:28:09 +0100

    add test case

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -3,6 +3,7 @@
 a text with 2 new lines
 after each of these characters: ., ? and :"""
 
+
 def text_indentation(text):
     """Print 2 new lines after .,?"""
     str_error = "text must be a string"
