a41e6e6ee74ad4023c64f117313efacf685ca995
zoraaga1 <fati.sousse12@gmail.com>
2023-11-02 01:10:40 +0100

    add test case

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -11,18 +11,10 @@ def text_indentation(text):
     flag = False
     if type(text) is not str:
         raise TypeError(str_error)
-    new_text = text.replace(". ", ".")
-    new_text = new_text.replace(": ", ":")
-    new_text = new_text.replace("? ", "?")
-    for char in new_text:
+    new_text = ""
+    for char in text:
+        new_text += char
         if char in [".", "?", ":"]:
-            print(char)
-            print()
-            flag = True
-        else:
-            if flag is False:
-                print(char, end="")
-            else:
-                if char != " ":
-                    print(char, end="")
-                    flag = False
+            new_text += "\n\n"
+
+    print(new_text)
