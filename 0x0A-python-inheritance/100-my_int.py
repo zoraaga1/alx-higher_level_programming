3183a879c2be0a8150b824c7caa8858a7df5ef23
zoraaga1 <fati.sousse12@gmail.com>
2023-11-07 00:31:24 +0100

    task 12

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Square class Module"""
+
+class MyInt:
+    """Class MyInt inherits from int"""
+    def __eq__(self, other):
+        """Inverts == operator"""
+        return super().__ne__(other)
+
+    def __ne__(self, other):
+        """Inverts != operator"""
+        return super().__eq__(other)
