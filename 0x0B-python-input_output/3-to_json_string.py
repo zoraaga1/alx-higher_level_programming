bdf852546086e0bfd5f2ced8cf403b51edae961c
zoraaga1 <fati.sousse12@gmail.com>
2023-11-07 17:34:07 +0100

    task 3

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 """Create o_json_string function"""
+
+
 import json
 
 
 def o_json_string(my_obj):
     """Returns the JSON representation of an object (string)"""
-    json_o = json.dumps(my_obj)
-    return json_o
+    return json.dumps(my_obj)
