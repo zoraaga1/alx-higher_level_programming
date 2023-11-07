5ffc2d04fd1b2e7ad20e30b2a1487fef6fe23d4c
zoraaga1 <fati.sousse12@gmail.com>
2023-11-07 18:33:46 +0100

    task 9

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -9,16 +9,17 @@ class Student:
         last_name
         age"""
     def __init__(self, first_name, last_name, age):
+        """Defines attr"""
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self):
         """Returns the dictionary description
-    with simple data structure"""
-    serializable_data = {}
-    for attr in obj.__dict__:
-        value = getattr(obj, attr)
-        if isinstance(value, (list, dict, str, int, bool)):
-            serializable_data[attr] = value
-    return serializable_data
+        with simple data structure"""
+        serializable_data = {}
+        for attr in self.__dict__:
+            value = getattr(self, attr)
+            if isinstance(value, (list, dict, str, int, bool)):
+                serializable_data[attr] = value
+        return serializable_data
