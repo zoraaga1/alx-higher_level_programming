3107c55cf37cd120c2e9f356d5eef49974ebb177
zoraaga1 <fati.sousse12@gmail.com>
2023-11-07 18:39:43 +0100

    task 10

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Create Student class """
+
+
+class Student:
+    """ defines a student by:
+    Public instance attributes:
+        first_name
+        last_name
+        age"""
+    def __init__(self, first_name, last_name, age):
+        """Defines attr"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Returns the dictionary description
+        with simple data structure"""
+        if attrs is not None and all(isinstance(attr, str) for attr in attrs):
+            return {attr: getattr(self, attr) for attr in attrs if hasattr(self, attr)}
+        else:
+            return self.__dict__
