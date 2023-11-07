bf8e03e86ff687e5bb881166a1bba40bc6fa4832
zoraaga1 <fati.sousse12@gmail.com>
2023-11-07 16:57:14 +0100

    task 0

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -4,9 +4,5 @@
 
 def read_file(filename=""):
     """Reads a text file (UTF8) and prints it to stdout:"""
-    try:
-        with open(filename, 'r', encoding='utf-8') as file:
-            for line in file:
-                print(line, end='')  # Printing each line without an additional newline
-    except Exception as e:
-        print("An error occurred:", e)
+    with open(filename, "r", encoding="UTF-8") as file:
+        print(file.read(), end="")
