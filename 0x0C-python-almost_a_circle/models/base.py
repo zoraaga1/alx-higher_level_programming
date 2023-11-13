726f9b7935213ff1b22343c556145c2a0200a6a4
zoraaga1 <fati.sousse12@gmail.com>
2023-11-13 20:32:51 +0100

    next tasks

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -40,3 +40,11 @@ class Base:
             json_string = cls.to_json_string([obj.to_dictionary()
                                               for obj in list_objs])
             file.write(json_string)
+
+    @staticmethod
+    def from_json_string(json_string):
+        """Returns the list of the JSON string representation json_string"""
+        if json_string is None or not json_string:
+            return []
+        else:
+            return json.loads(json_string)
