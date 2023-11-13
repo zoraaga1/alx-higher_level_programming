8468126126b83df9770215fe38b960d6e23c7bcf
zoraaga1 <fati.sousse12@gmail.com>
2023-11-13 20:20:51 +0100

    next tasks

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -33,9 +33,10 @@ class Base:
         """Writes the JSON string representation of list_objs to a file"""
         if list_objs is None:
             list_objs = []
-        
+
         filename = f"{cls.__name__}.json"
 
         with open(filename, 'w') as file:
-            json_string = cls.to_json_string([obj.to_dictionary() for obj in list_objs])
+            json_string = cls.to_json_string([obj.to_dictionary()
+                                              for obj in list_objs])
             file.write(json_string)
