b655cc666f0ee3ddcee0b4ce5bf2ca597f61dea6
zoraaga1 <fati.sousse12@gmail.com>
2023-11-13 20:36:19 +0100

    next tasks

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -48,3 +48,16 @@ class Base:
             return []
         else:
             return json.loads(json_string)
+
+    @classmethod
+    def create(cls, **dictionary):
+        """Returns an instance with all attributes already set"""
+        if cls.__name__ == "Rectangle":
+            dummy_instance = cls(1, 1)
+        elif cls.__name__ == "Square":
+            dummy_instance = cls(1)
+        else:
+            dummy_instance = None
+
+        dummy_instance.update(**dictionary)
+        return dummy_instance
