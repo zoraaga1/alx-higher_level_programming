6c3d38ffcf48bb94e030d03c572a21b8a9f219e3
zoraaga1 <fati.sousse12@gmail.com>
2023-12-12 22:04:00 +0100

    task 18

--- 0x0D-SQL_introduction/14-average.sql
+++ 0x0D-SQL_introduction/14-average.sql
@@ -1,3 +1,2 @@
 -- Computes the score average of all records in the table second_table of the database hbtn_0c_0 in your MySQL server.
-SELECT `city`, AVG(`value`) AS `avg_temp` FROM `temperatures`
-GROUP by `city` ORDER by `avg_temp` DESC;
+SELECT `city`, AVG(`value`) AS `avg_temp` FROM `temperatures` GROUP by `city` ORDER by `avg_temp` DESC;
