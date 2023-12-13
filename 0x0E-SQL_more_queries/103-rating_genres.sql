5a40c08cb468d45420d2a5ab2e3c76cccedae5a5
zoraaga1 <fati.sousse12@gmail.com>
2023-12-14 00:21:55 +0100

    task 13

--- 0x0E-SQL_more_queries/103-rating_genres.sql
+++ 0x0E-SQL_more_queries/103-rating_genres.sql
@@ -1,5 +1,5 @@
 -- Lists all genres in the database hbtn_0d_tvshows_rate by their rating.
-SELECT tv_genres.name, SUM(tv_shows_rate.rating) AS rating_sum FROM tv_genres
-LEFT JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
-LEFT JOIN tv_shows_rate ON tv_show_genres.show_id = tv_shows_rate.show_id
-GROUP BY tv_genres.name ORDER BY rating_sum DESC;
+SELECT name, SUM(tv_shows_rate.rating) AS 'rating_sum' FROM tv_genres
+INNER JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
+INNER JOIN tv_shows_ratings ON tv_show_genres.show_id = tv_shows_ratings.show_id
+GROUP BY name ORDER BY rating_sum DESC;
