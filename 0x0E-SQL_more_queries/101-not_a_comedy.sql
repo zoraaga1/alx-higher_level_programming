e1d7634960b16887c308b0ada029fe5fe0aab9a0
zoraaga1 <fati.sousse12@gmail.com>
2023-12-14 00:26:31 +0100

    task 18

--- 0x0E-SQL_more_queries/101-not_a_comedy.sql
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -1,8 +1,9 @@
 -- Lists all shows without the genre Comedy in the database hbtn_0d_tvshows.
-SELECT DISTINCT tv_genres.name FROM tv_genres WHERE tv_genres.id NOT IN (
-    SELECT tv_show_genres.genre_id
-    FROM tv_show_genres
-    INNER JOIN tv_shows ON tv_shows.id = tv_show_genres.show_id
-    WHERE tv_shows.title = 'Dexter'
+SELECT title FROM tv_shows WHERE title NOT IN (
+    SELECT title
+    FROM tv_show
+    LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+    LEFT JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
+    WHERE tv_genres.name = 'Dexter'
 )
-ORDER BY tv_genres.name;
+GROUP BY title ORDER BY title ASC;
