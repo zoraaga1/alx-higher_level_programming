6dfecd46351834fc87f7aebce2fade02f0ccb306
zoraaga1 <fati.sousse12@gmail.com>
2023-12-14 00:35:56 +0100

    task 19-20

--- 0x0E-SQL_more_queries/102-rating_shows.sql
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -1,4 +1,4 @@
 -- Lists all shows from hbtn_0d_tvshows_rate by their rating.
-SELECT title, SUM(tv_shows_ratings.rate) AS 'rating' FROM tv_shows
+SELECT title, SUM(tv_show_ratings.rate) AS 'rating' FROM tv_shows
 LEFT JOIN tv_shows_ratings ON tv_show_ratings.show_id = tv_shows.id
 GROUP BY title ORDER BY rating DESC;
