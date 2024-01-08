a607a72de5b3798e7c6812132e1fb9acb3e7dd21
zoraaaga <fati.sousse12@gmail.com>
2024-01-08 23:35:48 +0100

    task6

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-const arr1 = ["C is fun", "Python i cool", "JavaScript is amazing"];
+const arr1 = ["C is fun", "Python is cool", "JavaScript is amazing"];
 for (let i = 0; i < 3; i++) {
     console.log(arr1[i])
 }
