52d1a399010d472ca6dff793126a468326bcbc6e
zoraaaga <fati.sousse12@gmail.com>
2024-01-09 21:44:17 +0100

    task12

---
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+
+const { dict } = require('./101-data');
+
+const usersByOccurrence = {};
+
+for (const userId in dict) {
+  const occurrence = dict[userId];
+
+  if (!usersByOccurrence[occurrence]) {
+    usersByOccurrence[occurrence] = [];
+  }
+
+  usersByOccurrence[occurrence].push(userId);
+}
+
+console.log(usersByOccurrence);
