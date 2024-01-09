5bf1077ac0292aa6f1afc409b8516ef0609decca
zoraaaga <fati.sousse12@gmail.com>
2024-01-09 19:39:22 +0100

    task3

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,24 @@
+#!/usr/bin/node
+class Rectangle {
+  constructor (w, h) {
+    if (w == null || h == null) {
+      return;
+    }
+    if (w <= 0 || h <= 0) {
+      return;
+    }
+    this.width = w;
+    this.height = h;
+  }
+
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      let row = '';
+      for (let j = 0; j < this.width; j++) {
+        row += 'X';
+      }
+      console.log(row);
+    }
+  }
+}
+module.exports = Rectangle;
