cee6a7b27c8e3b5b2918d568b925d20378a89736
zoraaaga <fati.sousse12@gmail.com>
2024-01-09 21:14:00 +0100

    task8

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,16 +1,16 @@
 #!/usr/bin/node
 
 exports.esrever = function (list) {
-    let start = 0;
-    let end = list.length - 1;
+  let start = 0;
+  let end = list.length - 1;
 
-    while (start < end) {
-        let temp = list[start];
-        list[start] = list[end];
-        list[end] = temp;
+  while (start < end) {
+    const temp = list[start];
+    list[start] = list[end];
+    list[end] = temp;
 
-        start++;
-        end--;
-    }
-    return list;
+    start++;
+    end--;
+  }
+  return list;
 };
