99fe30cad0b601b64b1a81cb1cc425d8c8322c81
zoraaaga <fati.sousse12@gmail.com>
2024-01-12 23:33:48 +0100

    task5

---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+This script connects to a MySQL server,
+retrieves all cities from a specific database,
+and prints the results ordered by state ID.
+"""
+
+import MySQLdb
+from sys import argv
+
+
+def main():
+    """Main function to execute the script."""
+    db = MySQLdb.connect(host="localhost",
+                         port=3306, user=argv[1], passwd=argv[2], db=argv[3])
+    cursor = db.cursor()
+    cursor.execute("SELECT cities.id, cities.name, states.name FROM cities \
+             INNER JOIN states ON cities.state_id = states.id \
+             WHERE states.name LIKE %s ORDER BY cities.id ASC")
+    cities = cursor.fetchall()
+    list = []
+    for city in cities:
+        list.append(city[1])
+    print(", ".join(list))
+    cursor.close()
+    db.close()
+
+
+if __name__ == "__main__":
+    main()
