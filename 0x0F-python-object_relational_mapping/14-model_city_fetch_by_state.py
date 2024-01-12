e8f05067004e02004587947baddcf0f18419ff9e
zoraaaga <fati.sousse12@gmail.com>
2024-01-12 23:51:12 +0100

    task7

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -1,24 +1,29 @@
 #!/usr/bin/python3
 """
-Script that lists all State objects
-from the database hbtn_0e_6_usa
+Script that lists all State objects from the database - Using module SQLAlchemy
 """
 
+from model_state import Base, State
 from sys import argv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from model_state import Base, State
 
 if __name__ == "__main__":
-    engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}"
-                           .format(argv[1], argv[2], argv[3]), pool_pre_ping=True)
+    # create an engine
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+        argv[1], argv[2], argv[3]), pool_pre_ping=True)
 
+    # create a configured "Session" class
     Session = sessionmaker(bind=engine)
-
+    # create a Session
     session = Session()
 
+    # Query all State objects and order by states.id
     states = session.query(State).order_by(State.id).all()
 
+    # Display the results
     for state in states:
         print("{}: {}".format(state.id, state.name))
+
+    # Close the session
     session.close()
