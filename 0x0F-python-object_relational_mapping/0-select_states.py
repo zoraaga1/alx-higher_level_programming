98178a823795906bcf61b45ff287261067e8e287
zoraaaga <fati.sousse12@gmail.com>
2024-01-12 21:32:23 +0100

    task0

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,38 +1,26 @@
 #!/usr/bin/python3
 """
-This script connects to a MySQL server, retrieves all states from a specific database,
+This script connects to a MySQL server,
+retrieves all states from a specific database,
 and prints the results ordered by state IDs.
 """
 
 import MySQLdb
 from sys import argv
 
+
 def main():
     """Main function to execute the script."""
-    # Check if all three arguments are provided
-    if len(argv) != 4:
-        print("Usage: {} <username> <password> <database>".format(argv[0]))
-        exit(1)
-
-    # Connect to MySQL server
-    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1], passwd=argv[2], db=argv[3])
-
-    # Create a cursor object to execute SQL queries
+    db = MySQLdb.connect(host="localhost",
+                         port=3306, user=argv[1], passwd=argv[2], db=argv[3])
     cursor = db.cursor()
-
-    # Execute the SQL query to retrieve all states and order them by states.id
     cursor.execute("SELECT * FROM states ORDER BY states.id ASC")
-
-    # Fetch all the rows
     states = cursor.fetchall()
-
-    # Print the results
     for state in states:
         print(state)
-
-    # Close the cursor and database connection
     cursor.close()
     db.close()
+
 
 if __name__ == "__main__":
     main()
