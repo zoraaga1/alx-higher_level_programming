8a44b5c5e6637c03c7db584df04d8f1bfdec6ebd
zoraaaga <fati.sousse12@gmail.com>
2024-01-12 23:42:09 +0100

    task6

---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+Module contains the class definition of State
+and an instance Base = declarative_base()
+"""
+
+from sqlalchemy import Column, Integer, String, create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sys import argv
+
+Base = declarative_base()
+
+class State(Base):
+    """
+    Class State that inherits from Base.
+    Represents the states table in MySQL.
+    """
+
+    __tablename__ = 'states'
+
+    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
+    name = Column(String(128), nullable=False)
+
+if __name__ == "__main__":
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}".format(argv[1], argv[2], argv[3]))
+    Base.metadata.create_all(engine)
