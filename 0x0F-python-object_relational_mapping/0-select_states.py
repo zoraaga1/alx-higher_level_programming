808c76c991ffae2a66cc2945a59c6b7be1d33e45
zoraaaga <fati.sousse12@gmail.com>
2024-01-12 21:26:16 +0100

    task0

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,29 +1,12 @@
 #!/usr/bin/python3
+"""Displays all values in the states table of the database hbtn_0e_0_usa"""
 import MySQLdb
-from sys import argv
+import sys
 
 if __name__ == "__main__":
-    # Check if all three arguments are provided
-    if len(argv) != 4:
-        print("Usage: {} <username> <password> <database>".format(argv[0]))
-        exit(1)
-
-    # Connect to MySQL server
-    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1], passwd=argv[2], db=argv[3])
-
-    # Create a cursor object to execute SQL queries
-    cursor = db.cursor()
-
-    # Execute the SQL query to retrieve all states and order them by states.id
-    cursor.execute("SELECT * FROM states ORDER BY states.id ASC")
-
-    # Fetch all the rows
-    states = cursor.fetchall()
-
-    # Print the results
-    for state in states:
-        print(state)
-
-    # Close the cursor and database connection
-    cursor.close()
-    db.close()
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    cu = db.cursor()
+    cu.execute("SELECT * \
+                 FROM `states` \
+                WHERE BINARY `name` = '{}'".format(sys.argv[4]))
+    [print(state) for state in cu.fetchall()]
