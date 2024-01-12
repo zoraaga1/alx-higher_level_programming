290c2757ce3cdccc956aabc0c341782c952581b1
zoraaaga <fati.sousse12@gmail.com>
2024-01-12 21:29:31 +0100

    task0

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,17 +1,38 @@
 #!/usr/bin/python3
-# Displays all values in the states table of the database hbtn_0e_0_usa
-# whose name matches that supplied as argument.
-# Usage: ./2-my_filter_states.py <mysql username> \
-#                                <mysql password> \
-#                                <database name> \
-#                                <state name searched>
-import sys
+"""
+This script connects to a MySQL server, retrieves all states from a specific database,
+and prints the results ordered by state IDs.
+"""
+
 import MySQLdb
+from sys import argv
+
+def main():
+    """Main function to execute the script."""
+    # Check if all three arguments are provided
+    if len(argv) != 4:
+        print("Usage: {} <username> <password> <database>".format(argv[0]))
+        exit(1)
+
+    # Connect to MySQL server
+    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1], passwd=argv[2], db=argv[3])
+
+    # Create a cursor object to execute SQL queries
+    cursor = db.cursor()
+
+    # Execute the SQL query to retrieve all states and order them by states.id
+    cursor.execute("SELECT * FROM states ORDER BY states.id ASC")
+
+    # Fetch all the rows
+    states = cursor.fetchall()
+
+    # Print the results
+    for state in states:
+        print(state)
+
+    # Close the cursor and database connection
+    cursor.close()
+    db.close()
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
-    c = db.cursor()
-    c.execute("SELECT * \
-                 FROM `states` \
-                WHERE BINARY `name` = '{}'".format(sys.argv[4]))
-    [print(state) for state in c.fetchall()]
+    main()
