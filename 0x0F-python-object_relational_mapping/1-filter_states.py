41baa0e3e9693b77a46f8eedeb9d45d5f6de21a7
zoraaaga <fati.sousse12@gmail.com>
2024-01-12 21:54:17 +0100

    task1

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -14,16 +14,9 @@ def main():
     """Main function to execute the script."""
     db = MySQLdb.connect(host="localhost",
                          port=3306, user=argv[1], passwd=argv[2], db=argv[3])
-    cursor = db.cursor()
-    cursor.execute(
-        "SELECT * FROM states WHERE name LIKE 'N%' ORDER BY states.id ASC"
-    )
-    states = cursor.fetchall()
-    for state in states:
-        print(state)
-    cursor.close()
-    db.close()
-
+    cur = db.cursor()
+    cur.execute("SELECT * FROM `states` ORDER BY `id`")
+    [print(state) for state in cur.fetchall() if state[1][0] == "N"]
 
 if __name__ == "__main__":
     main()
