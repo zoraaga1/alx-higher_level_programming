e812fbc41329a7291aad3f02c9f2fbb10849dd87
zoraaaga <fati.sousse12@gmail.com>
2024-01-25 20:44:13 +0100

    tasks 6

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -11,13 +11,9 @@ def find_peak(list_of_integers):
     while low < high:
         mid = (low + high) // 2
 
-        # Check if mid is a peak
         if list_of_integers[mid] > list_of_integers[mid + 1]:
-            # The peak is on the left side
             high = mid
         else:
-            # The peak is on the right side (or mid is part of an ascending sequence)
             low = mid + 1
 
-    # At the end of the loop, low and high will converge to a peak
     return list_of_integers[low]
