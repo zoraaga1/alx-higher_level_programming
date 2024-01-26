f564199fa9e307ec13f8f4699c07ae85071213d3
zoraaaga <fati.sousse12@gmail.com>
2024-01-26 22:27:24 +0100

    task1

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -12,6 +12,6 @@ if __name__ == "__main__":
     with urlopen(url) as response:
         body = response.read()
         print("Body response:")
-        print("\t- type:", type(body))
+        print("\t- type: {}".format(type(body)))
         print("\t- content: {}".format(body))
         print("\t- utf8 content: {}".format(body.decode("utf-8")))
