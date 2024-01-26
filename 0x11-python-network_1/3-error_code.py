eeddde4554e88430a9a0822b9226b09757cd8288
zoraaaga <fati.sousse12@gmail.com>
2024-01-26 22:50:39 +0100

    task3

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+Takes in a URL, sends a request to the URL, and displays the body of the response (decoded in utf-8).
+Handles urllib.error.HTTPError exceptions and prints the HTTP status code.
+"""
+
+import urllib.request
+import urllib.error
+import sys
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+
+    try:
+        with urllib.request.urlopen(url) as response:
+            body = response.read()
+            print(body.decode('utf-8'))
+    except urllib.error.HTTPError as e:
+        print("Error code:", e.code)
