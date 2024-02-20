9c95dc663e7044f74e4a3f05920106309ff7d281
zoraaga1 <fati.sousse12@gmail.com>
2024-02-20 20:25:39 +0100

    tasks 2-3

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -4,5 +4,9 @@ const request = require('request');
 const url = process.argv[2];
 
 request(url, function (error, response) {
+  if (error) {
+    console.error(error);
+    return;
+  }
   console.log('code:', response.statusCode);
 });
--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,7 +1,8 @@
 #!/usr/bin/node
 
 const request = require('request');
-const url = 'https://swapi-api.alx-tools.com/api/films/:id';
+const movieId = process.argv[2];
+const url = `https://swapi-api.alx-tools.com/api/films//${movieId}`;
 
 request(url, (error, response, body) => {
   if (error) {
