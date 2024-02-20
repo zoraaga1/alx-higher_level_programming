b65425f7b03e6c579d44f4604f53d9108c36bb8e
zoraaga1 <fati.sousse12@gmail.com>
2024-02-20 21:23:10 +0100

    tasks 5

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -6,17 +6,10 @@ const fs = require('fs');
 const url = process.argv[2];
 const filePath = process.argv[3];
 
-request(url, (error, response, body) => {
-  if (error) {
-    console.error(error);
-    return;
-  }
-
-  fs.writeFile(filePath, body, { encoding: 'utf-8' }, (err) => {
-    if (err) {
-      console.error(err);
-      return;
-    }
-    console.log(`The contents of the webpage have been saved to ${filePath}`);
+request(url, function (_err, _res, body) {
+    fs.writeFile(filePath, body, 'utf8', function (err) {
+      if (err) {
+        console.log(err);
+      }
+    });
   });
-});
